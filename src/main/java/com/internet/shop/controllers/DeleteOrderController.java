dcfb5d68baeaea98199d9caf7fd181b4004860e8
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-09-11 10:17:53 +0300

    order controller added, delete from cart added

---
+++ src/main/java/com/internet/shop/controllers/DeleteOrderController.java
@@ -0,0 +1,22 @@
+package com.internet.shop.controllers;
+
+import com.internet.shop.library.Injector;
+import com.internet.shop.service.interfaces.OrderService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteOrderController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private OrderService orderService = (OrderService) injector.getInstance(OrderService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long orderId = Long.parseLong(req.getParameter("orderId"));
+        orderService.delete(orderId);
+        resp.sendRedirect(req.getContextPath() + "/orders/all");
+    }
+}
---
+++ src/main/java/com/internet/shop/controllers/DeleteProductFromCartController.java
@@ -0,0 +1,30 @@
+package com.internet.shop.controllers;
+
+import com.internet.shop.library.Injector;
+import com.internet.shop.model.ShoppingCart;
+import com.internet.shop.service.interfaces.ProductService;
+import com.internet.shop.service.interfaces.ShoppingCartService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteProductFromCartController extends HttpServlet {
+    private static final long USER_ID = 1;
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private ProductService productService =
+            (ProductService) injector.getInstance(ProductService.class);
+    private ShoppingCartService shoppingCartService
+            = (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ShoppingCart shoppingCart = shoppingCartService.getByUserId(USER_ID);
+        String productId = req.getParameter("productId");
+        shoppingCartService.deleteProduct(shoppingCart, productService.get(Long.parseLong(productId)));
+        resp.sendRedirect(req.getContextPath() + "/shopping-cart/products/");
+    }
+
+}
