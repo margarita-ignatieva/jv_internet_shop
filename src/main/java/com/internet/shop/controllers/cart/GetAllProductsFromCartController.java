6ad69b3f342996442417b1f1c3684a716c85ecc8
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-09-15 17:55:08 +0300

    USER_ID from session

--- src/main/java/com/internet/shop/controllers/cart/GetAllProductsFromCartController.java
+++ src/main/java/com/internet/shop/controllers/cart/GetAllProductsFromCartController.java
@@ -1,15 +1,17 @@
 package com.internet.shop.controllers.cart;
 
 import com.internet.shop.library.Injector;
+import com.internet.shop.model.Product;
 import com.internet.shop.service.interfaces.ShoppingCartService;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class GetAllProductsFromCartController extends HttpServlet {
-    private static final Long USER_ID = 1L;
+    private static final String USER_ID = "user_id";
     private static final Injector injector = Injector.getInstance("com.internet.shop");
     private ShoppingCartService shoppingCartService =
             (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
@@ -17,7 +19,8 @@ public class GetAllProductsFromCartController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.setAttribute("products", shoppingCartService.getByUserId(USER_ID).getProducts());
+        Long userId = (Long) req.getSession().getAttribute(USER_ID);
+        List<Product> addedProducts = shoppingCartService.getByUserId(userId).getProducts();
         req.getRequestDispatcher("/WEB-INF/views/shoppingCart/allProducts.jsp").forward(req, resp);
     }
 }
--- src/main/java/com/internet/shop/controllers/order/CompleteOrderController.java
+++ src/main/java/com/internet/shop/controllers/order/CompleteOrderController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CompleteOrderController extends HttpServlet {
-    private static final Long USER_ID = 1L;
+    private static final String USER_ID = "user_id";
     private static final Injector injector = Injector.getInstance("com.internet.shop");
     private ShoppingCartService shoppingCartService =
             (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
@@ -21,7 +21,8 @@ public class CompleteOrderController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        ShoppingCart shoppingCart = shoppingCartService.getByUserId(USER_ID);
+        Long userId = (Long) req.getSession().getAttribute(USER_ID);
+        ShoppingCart shoppingCart = shoppingCartService.getByUserId(userId);
         orderService.completeOrder(shoppingCart);
         resp.sendRedirect(req.getContextPath() + "/user/orders");
     }
