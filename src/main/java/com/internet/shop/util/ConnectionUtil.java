643d06502d61abaeddd75d14ce4683ea69d2ff70
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-09-20 14:47:14 +0300

    ConnectionUtil added

---
+++ src/main/java/com/internet/shop/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package com.internet.shop.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find MySQL driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.put("user","rita");
+        dbProperties.put("password","root");
+        String dbUrl = "jdbc:mysql://localhost:3306";
+
+        try {
+            return DriverManager.getConnection(dbUrl, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't connect to DB", e);
+        }
+    }
+}
